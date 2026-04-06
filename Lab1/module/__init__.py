4214690dba4ebf0644ebd26fb4ddf497224c38a0
Icedpiggy <icedpiggy@163.com>
2026-04-06 21:52:40 +0800

    Lab1 __init__

---
+++ Lab1/module/__init__.py
@@ -0,0 +1,15 @@
+from .core import Module, Optimizer
+from .init import xavier_uniform, he_normal
+from .layers import Linear, BatchNorm, Dropout
+from .activations import ReLU, Sigmoid
+from .losses import CrossEntropyLoss, MSELoss, MAELoss
+from .optimizers import SGD, SGDM, Adam
+
+__all__ = [
+	'Module', 'Optimizer',
+	'xavier_uniform', 'he_normal',
+	'Linear', 'BatchNorm', 'Dropout',
+	'ReLU', 'Sigmoid',
+	'CrossEntropyLoss', 'MSELoss', 'MAELoss',
+	'SGD', 'SGDM', 'Adam',
+]
