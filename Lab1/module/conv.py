9dec0971963cfb3b5b7a41659c17dfb72784226e
Icedpiggy <icedpiggy@163.com>
2026-04-10 19:57:37 +0800

    Lab1 task2 MaxPool2d simplifiy

--- Lab1/module/conv.py
+++ Lab1/module/conv.py
@@ -3,18 +3,28 @@ from .core import *
 from numpy.lib.stride_tricks import as_strided
 
 class Conv2d(Module):
-	pass
+	def __init__(self):
+		pass
+	def im2col(self, x):
+		pass
+	def col2im(self, x):
+		pass
+	def forward(self, x):
+		pass
+	def backward(self, x):
+		pass
+
 
 class MaxPool2d(Module):
 	def __init__(self, kernel_size, stride=None):
 		super().__init__()
 		self.kernel_size = kernel_size
 		self.stride = stride if stride else kernel_size
-		self.x_shape = None
+		self.H = None
+		self.W = None
 		self.max_idx = None
 
 	def forward(self, x):
-		self.x_shape = x.shape
 		batch_size, channels, H, W = x.shape
 		out_h = (H - self.kernel_size) // self.stride + 1
 		out_w = (W - self.kernel_size) // self.stride + 1
@@ -23,35 +33,33 @@ class MaxPool2d(Module):
 			shape=(batch_size, channels, out_h, out_w, self.kernel_size, self.kernel_size),
 			strides=(x.strides[0], x.strides[1], x.strides[2] * self.stride, x.strides[3] * self.stride, x.strides[2], x.strides[3])
 		)
-		windows = windows.reshape(windows.shape[0], windows.shape[1], windows.shape[2], windows.shape[3], -1)
+		windows = windows.reshape(batch_size, channels, out_h, out_w, -1)
 		max_val = np.max(windows, axis=-1)
-		self.max_idx = np.argmax(windows, axis=-1)
+		if self.training:
+			self.H, self.W = H, W
+			self.max_idx = np.argmax(windows, axis=-1)
 		return max_val
 
 	def backward(self, d):
 		batch_size, channels, out_h, out_w = d.shape
-		batch_size, channels, H, W = self.x_shape
-		out_idx_bias = np.arange(H * W).reshape(H, W)[::self.stride, ::self.stride]
-		kernel_idx_bias = np.arange(self.kernel_size).reshape(-1, 1) * W + np.arange(self.kernel_size).reshape(1, -1)
-		in_idx = out_idx_bias.reshape(out_h, out_w, 1) + kernel_idx_bias.reshape(1, 1, -1)
+		H, W = self.H, self.W
+
+		h_kernel = self.max_idx // self.kernel_size
+		w_kernel = self.max_idx % self.kernel_size
+
+		n_idx = np.broadcast_to(np.arange(batch_size).reshape(-1, 1, 1, 1), d.shape)
+		c_idx = np.broadcast_to(np.arange(channels).reshape(1, -1, 1, 1), d.shape)
+		h_idx = np.arange(out_h).reshape(1, 1, -1, 1) * self.stride + h_kernel
+		w_idx = np.arange(out_w).reshape(1, 1, 1, -1) * self.stride + w_kernel
 		
-		in_idx = np.broadcast_to(in_idx, (batch_size, channels, out_h, out_w, self.kernel_size * self.kernel_size))
-		in_max_idx = in_idx[
-			np.arange(batch_size)[:, None, None, None],
-			np.arange(channels)[None, :, None, None],
-			np.arange(out_h)[None, None, :, None],
-			np.arange(out_w)[None, None, None, :],
-			self.max_idx
-   		]
-		in_max_idx = in_max_idx.reshape(batch_size, channels, -1)
-		
-		d_out = np.zeros((batch_size, channels, H * W))
-		d_out[
-			np.arange(batch_size)[:, None, None],
-			np.arange(channels)[None, :, None],
-			in_max_idx
-		] += d.reshape(batch_size, channels, -1)[:, :, :]
-		d_out = d_out.reshape(batch_size, channels, H, W)
+		n_flat = n_idx.flatten()
+		c_flat = c_idx.flatten()
+		h_flat = h_idx.flatten()
+		w_flat = w_idx.flatten()
+		d_flat = d.flatten()
+
+		d_out = np.zeros((batch_size, channels, H, W))
+		d_out[n_flat, c_flat, h_flat, w_flat] = d_flat
 
 		self.x_shape = None
 		self.max_idx = None
