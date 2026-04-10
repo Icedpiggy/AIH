61753e467a2ecdda34465995e013951361c5b214
Icedpiggy <icedpiggy@163.com>
2026-04-10 18:03:11 +0800

    Lab1 task2 MaxPool2d

--- Lab1/module/__init__.py
+++ Lab1/module/__init__.py
@@ -4,6 +4,7 @@ from .layers import Linear, BatchNorm, Dropout
 from .activations import ReLU, Sigmoid
 from .losses import CrossEntropyLoss, MSELoss, MAELoss
 from .optimizers import SGD, SGDM, Adam
+from .conv import Conv2d, MaxPool2d
 
 __all__ = [
 	'Module', 'Optimizer',
@@ -12,4 +13,5 @@ __all__ = [
 	'ReLU', 'Sigmoid',
 	'CrossEntropyLoss', 'MSELoss', 'MAELoss',
 	'SGD', 'SGDM', 'Adam',
+    'Conv2d', 'MaxPool2d'
 ]
